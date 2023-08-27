a06efd3731fb9e8176e54ba89c8f30ac3070cb7c
ryan-ahn <kaaiinn4@gmail.com>
2023-08-28 01:04:57 +0900

    Add: 스캐너 추가

--- App.js
+++ App.js
@@ -1,11 +1,49 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+import { BarCodeScanner } from 'expo-barcode-scanner';
+import axios from 'axios';
 
 export default function App() {
+  const [hasPermission, setHasPermission] = useState(null);
+  const [scanned, setScanned] = useState(false);
+  const [data, setData] = useState(null)
+
+  const handleBarCodeScanned = ({ data }) => {
+    if (hasPermission && !scanned) {
+      console.log(data)
+      setScanned(true)
+      axios.post(`https://hocalhost:8080/v1/common/detail/qr`, {userId: data})
+    }
+  };
+
+  useEffect(() => {
+    const getBarCodeScannerPermissions = async () => {
+      const { status } = await BarCodeScanner.requestPermissionsAsync();
+      setHasPermission(status === 'granted');
+    };
+    getBarCodeScannerPermissions();
+  }, []);
+
+  useEffect(() => {
+    if (hasPermission && scanned) {
+      setTimeout(() => {
+        setScanned(false)
+      }, 2000)
+    }
+  },[scanned])
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <BarCodeScanner
+        onBarCodeScanned={handleBarCodeScanned}
+        style={StyleSheet.absoluteFillObject}
+      />
+      {scanned && 
+        <View style={styles.modal}>
+          <Text style={styles.modalText}>반갑습니다</Text>
+          <Text style={styles.modalDescription}>{`${data}님`}</Text>
+        </View>
+      }
     </View>
   );
 }
@@ -13,8 +51,28 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    padding: 24,
+    backgroundColor: '#eaeaea',
   },
+  modal: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: '80%',
+    height: 150,
+    borderRadius: 20,
+    gap: 15,
+    backgroundColor: 'white'
+  },
+  modalText: {
+    color: '#333',
+    fontSize: 20,
+    fontWeight: 'bold',
+  },  
+  modalDescription: {
+    color: '#777',
+    fontSize: 40,
+    fontWeight: 'light',
+  }
 });
